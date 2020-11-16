b19d363080af37dcbf3dfa0c3185e83996f4ba39
fredrikl <fredrikl@kahoot.com>
2020-11-16 21:46:44 +0100

    add testdate generation

--- lightgbm/test_data/test_data_gen.py
+++ lightgbm/test_data/test_data_gen.py
@@ -9,31 +9,31 @@ from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.metrics import mean_squared_error,roc_auc_score,precision_score,recall_score
 import json
 
-pd.options.display.max_columns = 999
 
-X=load_breast_cancer()
-df=pd.DataFrame(X.data,columns=X.feature_names)
-Y=X.target
-sc=StandardScaler()
-sc.fit(df)
 
-X=pd.DataFrame(sc.fit_transform(df))
-X_train,X_test,y_train,y_test=train_test_split(X,Y,test_size=0.3,random_state=0)
-d_train=lgb.Dataset(X_train, label=y_train)
+def test_binary():
+    X, y = load_breast_cancer(return_X_y=True)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+    gbm = lgb.LGBMClassifier(n_estimators=50, silent=True)
+    gbm.fit(X_train, y_train, eval_set=[(X_test, y_test)], early_stopping_rounds=5, verbose=False)
+    y_pred = gbm.predict(X_test)
+    y_pred = [ 1 if y > 0.5 else 0 for y in y_pred]
 
-params={}
-params['learning_rate']=0.03
-params['boosting_type']='gbdt' #GradientBoostingDecisionTree
-params['objective']='binary' #Binary target feature
-params['metric']='binary_logloss' #metric for binary classification
-params['max_depth']=10
-
-clf=lgb.train(params,d_train,100) #train the model on 100 epocs
-y_pred = [ 1 if y > 0.5 else 0 for y in clf.predict(X_test)]
-
-print(f'test precision : {precision_score(y_test, y_pred)}\
+    print(f'test precision : {precision_score(y_test, y_pred)}\
         test recall : {recall_score(y_test, y_pred)}\
         test auc:{roc_auc_score(y_test, y_pred)}')
 
-with open('gbdt.json', 'w') as gbdtfile:
-    json.dump(clf.dump_model(), gbdtfile, indent = 4)
+    with open('gbdt-data.txt', 'w') as gbdtdatafile:
+        for p in X_test[:100].tolist():
+            print(f"{' '.join([ str(n) for n in p])}", file=gbdtdatafile)
+
+    with open('gbdt-data-pred-leaves.txt', 'w') as gbdtdata_pred_leaves_file:
+        for p in gbm.predict(X_test[:100], pred_leaf=True).tolist():
+            print(f"{' '.join([ str(n) for n in p])}", file=gbdtdata_pred_leaves_file)
+
+    gbm.booster_.save_model('gbdt.txt')
+    with open('gbdt.json', 'w') as gbdtfile:
+        json.dump(gbm.booster_.dump_model(), gbdtfile, indent = 4)
+
+if __name__ == '__main__':
+    test_binary()
